4727136d018df3ef39d777df7fbe917f1ae246c1
Taylor Waddell <74875060+taylorwaddell@users.noreply.github.com>
2021-02-10 14:46:47 -0600

    Initial Commit

---
+++ test-template.js
@@ -0,0 +1,20 @@
+describe("FuncionNameHere()", () => {
+    describe("Test Category 1:", () => {
+        it("test", () => {
+        });
+        it("test", () => {
+        });
+    });
+    describe("Test Category 2:", () => {
+        it("test", () => {
+        });
+        it("test", () => {
+        });
+    });
+    describe("Test Category 3:", () => {
+        it("test", () => {
+        });
+        it("test", () => {
+        });
+    });    
+});
